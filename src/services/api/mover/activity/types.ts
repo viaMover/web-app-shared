52585a6b0e585693b18a9213029070d1e8b5c715
Andrew Gergert <axedrew33@gmail.com>
2022-12-19 22:36:45 +0600

    Use narrower data type

--- src/services/api/mover/activity/types.ts
+++ src/services/api/mover/activity/types.ts
@@ -1,10 +1,20 @@
 import { Network } from '../../../../references/network';
 import { SmallTokenInfo } from '../../../../references/tokens';
+import { TransactionType } from '../transactions/types';
 
-export type TxDataDto = {
+export type TxData = {
   networkFrom: Network;
   networkTo: Network;
   amount: string;
   token: SmallTokenInfo;
   hash: string;
+};
+
+export type TxDataDto = {
+  chainIdFrom: number;
+  chainIdTo: number;
+  amount: string;
+  token: Omit<SmallTokenInfo, 'network'>;
+  hash: string;
+  type: TransactionType;
 };
